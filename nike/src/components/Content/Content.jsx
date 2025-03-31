80f1542b3e5a01f54a7c056fcce2aa0ad4872be1
Your NamePratham <pra172005@gmail.com>
2025-04-01 03:06:26 +0530

    content

--- nike/src/components/Content/Content.jsx
+++ nike/src/components/Content/Content.jsx
@@ -4,59 +4,45 @@ import air1 from './shoes/air1.png';
 
 const Content = () => {
   return (
-    <div className='flex gap-5 w-screen overflow-hidden border-white border-2 content-container text-white'>
+    <div className='flex gap-5 w-screen overflow-hidden border-white border-2 content-container text-white z-10'>
 
-      <div className='flex flex-col justify-center gap-4 px-20 w-1/2'>
-
-        <span className='text-4xl font-black '>Air Jordan 1 'Black Toe Reimagined'</span>
-
-        <span className='text-2xl font-normal'>
+      <div className='flex flex-col justify-center gap-8 px-10 w-1/2'>
+        <span className='text-5xl font-black '>Air Jordan 1 'Black Toe Reimagined'</span>
+        <span className='text-xl font-normal'>
           Scheduled for release on February 1, 2025, this iteration reimagines the classic 'Black Toe' design. It
           combines the iconic black, red, and white color scheme with modern materials and construction, offering a
           fresh take on a beloved classic.
         </span>
-
-        <span className='text-6xl font-semibold px-6'>
-          ₹15,000
-        </span>
-      </div>
-
-
-      {/* <div className='w-2/3 py-30 border-2 border-amber-50 relative'>
- 
- 
-        <div className="absolute text-white text-9xl font-extrabold opacity-40 rotate-340 flex flex-col justify-center px-50">
-        <span>Just Do It</span>
-        <span>Just Do It</span>
-        <span>Just Do It</span>
+        <div className='flex flex-col gap-10' >
+          <span className='text-7xl font-semibold'>
+            ₹15,000
+          </span>
+          <div className='flex gap-8 px-10 py-2'>
+            <button className='text-2xl border-white border-2 rounded-2xl px-5 py-2'>Buy Now</button>
+            <button className='text-2xl border-white border-2 rounded-2xl px-5 py-2'>Add To Cart</button>
+          </div>
         </div>
-
-        <img
-          src={air1}
-          alt="air1"
-          className="scale-110 rotate-[340deg] relative z-10 px-20"
-        />
       </div>
- */}
-      <div className='w-2/3 border-2 border-amber-50 relative'>
-     
-     
-        <div className="absolute text-white font-extrabold opacity-40 flex flex-col justify-center px-50 border-2 border-white">
-          <span className="stroke-text text-8xl">Just Do It</span>
-          <span className="glow-text text-9xl ">Just Do It</span>
-          <span className="stroke-text text-8xl">Just Do It</span>
+
+      <div className='w-[60%] relative flex items-center justify-center '>
+        <div className="absolute text-white font-extrabold opacity-40 flex flex-col items-center justify-center flex-nowrap px-50">
+          <span className="stroke-text text-8xl">Just Do It.</span>
+          <span className="stroke-text text-8xl">Just Do It.</span>
+          <span className="glow-text text-8xl">Just Do It.</span>
+          <span className="glow-text text-[120px]">Just Do It.</span>
+          <span className="glow-text text-8xl">Just Do It.</span>
+          <span className="stroke-text text-8xl">Just Do It.</span>
+          <span className="stroke-text text-8xl">Just Do It.</span>
         </div>
-
-        <img
-          src={air1}
-          alt="air1"
-          className="scale-110 rotate-[340deg] relative z-10 px-20"
-        />
+        <div className='px-10'>
+          <img
+            src={air1}
+            alt="air1"
+            className="scale-110 rotate-[330deg] relative z-10 "
+          />
+        </div>
       </div>
-
-
     </div>
-
   );
 };
 
